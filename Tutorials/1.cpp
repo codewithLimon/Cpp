22ba8b6f26871f025c51946433743b11441308ff
Kaium Al Limon <limon.3cs@gmail.com>
2023-03-12 19:26:54 +0600

    Cpp practice 1

---
+++ Tutorials/1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+const double pi=3.1416;
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    std::cout << "Pi values: "<<pi<< std::endl;
+    return 0;
+}
---
+++ Tutorials/2.c
@@ -0,0 +1,8 @@
+// #include <stdio.h>
+// #include <windows.h>
+// const double pi=3.1416;
+// int main()
+// {
+//     printf("Value of pi: %lf",);
+//     return 0;
+// }
