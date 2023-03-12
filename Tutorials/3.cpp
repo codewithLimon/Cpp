bb120a034b92821c6d0690aabc88e0313a65518a
Kaium Al Limon <limon.3cs@gmail.com>
2023-03-12 19:32:07 +0600

    practice 2

---
+++ Tutorials/3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main(){
+    int a=10;
+    int b=20;
+
+    std::cout << "a-b: "<<a-b << std::endl;
+
+    float c=10.50;
+    float d=5.25;
+
+    std::cout << "c+d: "<<c+d << std::endl;
+}
